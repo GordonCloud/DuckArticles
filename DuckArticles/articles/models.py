1676bf2a1ff391fe66dcc949a76229d42da97181
GordonCloud <gordoncloud@inbox.ru>
2023-01-02 15:58:53 +0300

    Added models for articles, urls and view for tests

--- DuckArticles/articles/models.py
+++ DuckArticles/articles/models.py
@@ -1,3 +1,40 @@
-from django.db import models
+import os
 
-# Create your models here.
+from django.db import models
+from django.core.exceptions import ObjectDoesNotExist
+
+from .imageStorage import OverwriteStorage
+
+
+def get_file_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (instance.id, ext)
+    return os.path.join('block-images', filename)
+
+
+class Article(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(max_length=1000)
+    image = models.ImageField(upload_to='images/', blank=True)
+    file_html = models.FileField(upload_to='articles/html/', blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ArticleBlock(models.Model):
+    article = models.ForeignKey(Article, related_name='%(class)s', on_delete=models.CASCADE)
+    type = models.CharField(max_length=25)
+
+    class Meta:
+        abstract = True
+
+
+class ImageBlock(ArticleBlock):
+    image = models.ImageField(max_length=500, upload_to=get_file_path, storage=OverwriteStorage(), blank=True)
+
+    directory_string_var = 'block-images'
+
+
+class TextBlock(ArticleBlock):
+    description = models.TextField(max_length=1000)
