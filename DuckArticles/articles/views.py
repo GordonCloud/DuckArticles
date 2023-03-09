1629c29cdd9d9aba308c24416788179c7ad047f1
Преображенский Севериан <spreobrazhensky@data-platform.ru>
2023-03-09 09:35:37 +0300

    Added tinymce upload handler. There is the stub instead of storage for now

--- DuckArticles/articles/views.py
+++ DuckArticles/articles/views.py
@@ -1,5 +1,14 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.template import RequestContext
+import json
+import tinymce
 
 
 def mainpage(request):
     return HttpResponse('This is the mainpage!')
+
+
+def savetmceimage(request):
+    file = request.FILES.get('file')
+    return HttpResponse('file', RequestContext(request))
+    # return JsonResponse({'location': 'folder/sub-folder/new-location.png'})
